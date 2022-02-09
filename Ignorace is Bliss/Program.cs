aa0e86fae6d8003d5fb0407798107c03daabfa9a
Steve Jones <jones.wiredbraincoffee@gmail.com>
2022-02-08 20:38:39 -0500

    Save #1

--- Ignorace is Bliss/Program.cs
+++ Ignorace is Bliss/Program.cs
@@ -9,8 +9,14 @@ namespace Ignorace_is_Bliss
     class Program
     {
         public static Player currentPlayer = new Player();
+
+       
         static void Main(string[] args)
+     
         {
+            //Set the Background Color and Foreground Color
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Blue;
             Start();
         }
 
@@ -20,19 +26,24 @@ namespace Ignorace_is_Bliss
         {
             Console.WriteLine($"Ignorance is Bliss");
             Console.WriteLine("Please insert a name:");
+            Console.ForegroundColor = ConsoleColor.White;
             currentPlayer.name = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.Clear();
             Console.WriteLine($"You wake to the sound of rain pitter pattering on the side of the car's window." );
             Console.WriteLine($"Interstate lights flashing along the back of the car along your face, as your strapped in tightly in your carseat.");
             Console.WriteLine($"It still remains wholly dark outside. \n");
             Console.WriteLine($"Your mother notices you sleepily moving your head, making eye contact with you in the front mirror. \n");
             if(currentPlayer.name=="")
             {
+                Console.ForegroundColor = ConsoleColor.Cyan;
                 Console.WriteLine($"'Hey sweetie did you sleep well?'");
             }
             else
-
-            Console.WriteLine($"'Hey " +currentPlayer.name + " did you sleep well?'");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine($"'Hey " +currentPlayer.name + " did you sleep well?'\n");
             Console.ReadKey();
+            Console.Clear();
         }
     }
 }
