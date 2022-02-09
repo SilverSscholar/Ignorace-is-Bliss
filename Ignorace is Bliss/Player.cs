83e3ab99af6b57bafb9cbeb4f01efa96beaf13c4
Steve Jones <jones.wiredbraincoffee@gmail.com>
2022-02-08 20:21:43 -0500

    Add project files.

---
+++ Ignorace is Bliss/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ignorace_is_Bliss
+{
+    class Player
+    {
+        public string name;
+        public int healing =5;
+        public int health = 10;
+        public int damage =1;
+        
+    }
+}
---
+++ Ignorace is Bliss/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ignorace_is_Bliss
+{
+    class Program
+    {
+        public static Player currentPlayer = new Player();
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+
+
+        static void Start() //happens seperate from the main string arguments; game start
+        {
+            Console.WriteLine($"Ignorance is Bliss");
+            Console.WriteLine("Please insert a name:");
+            currentPlayer.name = Console.ReadLine();
+            Console.WriteLine($"You wake to the sound of rain pitter pattering on the side of the car's window." );
+            Console.WriteLine($"Interstate lights flashing along the back of the car along your face, as your strapped in tightly in your carseat.");
+            Console.WriteLine($"It still remains wholly dark outside. \n");
+            Console.WriteLine($"Your mother notices you sleepily moving your head, making eye contact with you in the front mirror. \n");
+            if(currentPlayer.name=="")
+            {
+                Console.WriteLine($"'Hey sweetie did you sleep well?'");
+            }
+            else
+
+            Console.WriteLine($"'Hey " +currentPlayer.name + " did you sleep well?'");
+            Console.ReadKey();
+        }
+    }
+}
